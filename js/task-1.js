3f8884de9775d6c0c6ef30c0f89a660d55043b49
Pavlina-Sorochak <143879103+Pavlina-Sorochak@users.noreply.github.com>
2023-11-23 19:54:35 +0200

    Create task-1.js

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+function slugify(title) {
+const wrds = title.toLowerCase().split(' ');
+const slug = wrds.join('-');
+return slug;
+}
+
+
+
+console.log(slugify("Arrays for begginers")); 
+console.log(slugify("English for developer")); 
+console.log(slugify("Ten secrets of JavaScript")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
